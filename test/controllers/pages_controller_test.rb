f95487f80b5262382f188929ff96dcb790ecec69
Kat Drobnjakovic <kat.drobnjakovic@gmail.com>
2014-08-13 22:49:43 -0400

    Added a pages controller

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
+    # test "the truth" do
+  	#   assert true
+ 	# end
   end
 
 end
